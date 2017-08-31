b8d45582f500f201dd78b82b8c8f3485b704da4c
Hack Reactor Student <student@hackreactor.com>
2017-08-31 12:43:06 -0500

    hooked up youtube api

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -4,14 +4,16 @@ var searchYouTube = (options, callback) => {
 // See full sample for buildApiRequest() code, which is not
 // specific to a particular youtube or youtube method.
   $.ajax({
-    type: 'GET',
     url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
     data: {'maxResults': options.max,
      'q': options.query,
      'key': options.key,
      'part': 'snippet',
      'type': 'video'},
-    success: function(data) {callback(data.items);},
+    success: function(data) {
+      callback(data.items)
+    },
     dataType: 'json'
   });
 
